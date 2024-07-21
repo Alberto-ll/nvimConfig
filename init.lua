a42aaf81039a5370fc02b856724ee0c5ef1a25ae
Alberto-ll <Unityalberto.lier@gmail.com>
2024-07-21 11:20:26 -0300

    ningun cambio

--- init.lua
+++ init.lua
@@ -1,12 +1,15 @@
 --vim.cmd('set shell=powershell')
 --vim.o.shell = 'powershell.exe'
-
-
 require('core.baseConf')
 require('plugins.plug-vim')
 require('plugins.nerdtree')
 require('core.keyMaps')
 require('plugins.theme')
+
+
 require('plugins.Coc')
 require('plugins.airline')
 require('plugins.toggleterm')
+
+
+
