a774114a6dbf12524d1c521028f1da04c75e9cd3
Alberto-ll <Unityalberto.lier@gmail.com>
2024-07-22 23:14:06 -0300

    cambio minimo

--- lua/plugins/Coc/coc-extensions.lua
+++ lua/plugins/Coc/coc-extensions.lua
@@ -1,3 +1,3 @@
 -- Aca se listan las extenciones que se deben instalar o que se utilizan
 
-vim.cmd("let g:coc_global_extensions = ['coc-markdownlint','coc-sumneko-lua','coc-pyright']")
+--vim.cmd("let g:coc_global_extensions = ['coc-markdownlint','coc-sumneko-lua','coc-pyright']")
